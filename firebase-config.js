a6414ad601b86161f4874c06e99f4b814e42b163
haroldferreira98 <haroldferreira98@gmail.com>
2026-09-15 14:18:04 +0200

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,10 @@
 // Configuration publique de l'application Web Firebase.
-// Remplacez null par l'objet firebaseConfig fourni par la console Firebase.
-window.BUDGET_FIREBASE_CONFIG = null;
+window.BUDGET_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDsN9i87GGzx_v_Ci72yt-oDWN77S2Apak",
+  authDomain: "mybudgetmanagerapplication.firebaseapp.com",
+  projectId: "mybudgetmanagerapplication",
+  storageBucket: "mybudgetmanagerapplication.firebasestorage.app",
+  messagingSenderId: "83517729033",
+  appId: "1:83517729033:web:07144c82e6f6bab3984bd8",
+  measurementId: "G-YHNCYY4FYK"
+};
